8dbb31be83fc58bf70888f11c6ba7a31cf44fc64
matthewferreira <matthewferreira@ECS-ASH.saclink.csus.edu>
2020-11-25 22:53:37 -0800

    working on drawing tomatos

---
+++ A2prj/src/com/mycompany/myapp/IDrawable.java
@@ -0,0 +1,8 @@
+package com.mycompany.myapp;
+
+import com.codename1.charts.models.Point;
+import com.codename1.ui.Graphics;
+
+public interface IDrawable {
+	public void draw(Graphics g, Point pCmpRelPrnt);
+}
